dbb230ef78348787d26aaf1d2da601fbdfcc6948
chae <jungchae5812@sookmyung.ac.kr>
2022-08-28 10:01:33 +0900

    recyclerview 미완2

--- app/src/main/java/com/example/team16/BuyFragment.java
+++ app/src/main/java/com/example/team16/BuyFragment.java
@@ -1,19 +1,67 @@
 package com.example.team16;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ProgressBar;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.team16.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class BuyFragment extends Fragment {
 
-    @Nullable
+    private RecyclerView recyclerView;
+    private BuyAdapter mAdapter;
+    private ArrayList<BuyData> buylist = new ArrayList<>();
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_buy, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.activity_buy, container, false);
+
+        //recyclerview
+        recyclerView = (RecyclerView) view.findViewById(R.id.product_list);
+        recyclerView.setHasFixedSize(true);
+        mAdapter = new BuyAdapter(buylist);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
     }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        prepareData();
+    }
+
+    //데이터 준비(최종적으로는 동적으로 추가하거나 삭제할 수 있어야 한다. 이 데이터를 어디에 저장할지 정해야 한다.)
+    private void prepareData() {
+        buylist.add(new BuyData("서울시청","37.54892296550104"));
+        buylist.add(new BuyData("경복궁","126.99089033876304"));
+        buylist.add(new BuyData("서울역","37.54892296550104"));
+        buylist.add(new BuyData("남산","4892296550104"));
+        buylist.add(new BuyData("을지로입구역","37.54892296550104"));
+    }
+
 }
